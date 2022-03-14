f8f0d64cdcdd94fd28f48419145679fff8f788cc
Giulio Pedicone <pediconegiulio02@gmail.com>
2022-03-14 10:49:24 +0100

    Aggiunti gli appunti del 14 marzo 2022

---
+++ 14-03-2022/ricorsione.cpp
@@ -0,0 +1,119 @@
+/**************************
+ *     TORRI DI HANOI
+ **************************/
+#include <iostream>
+using namespace std;
+
+int succ(int n) // Dimostrazione per induzione
+{
+    if (n == 0) // caso base
+    {
+        return 1;
+    }
+    else // passo induttivo
+    {
+        return 1 + succ(n - 1);
+    }
+}
+
+int somma(int a, int b)
+{
+    if (b == 1)
+    {
+        return succ(a);
+    }
+    else
+    {
+        return somma(a, b - 1) + 1;
+    }
+
+    return 0;
+}
+
+int prodotto(int a, int b)
+{
+    if (b == 1)
+    {
+        return a;
+    }
+    else
+    {
+        return somma(a, prodotto(a, b - 1));
+    }
+}
+
+int potenza(int a, int b)
+{
+    if (b == 1)
+    {
+        return a;
+    }
+    else
+    {
+        return prodotto(a, potenza(a, b - 1)); // a * pow(a, b-1)
+    }
+}
+
+int fattoriale(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return prodotto(n, fattoriale(n - 1));
+    }
+}
+
+int fibonacci(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return somma(fibonacci(n - 1), fibonacci(n - 2));
+    }
+}
+
+int fibonacci_iterativa(int n)
+{
+    int sum1 = 1; // Fibonacci fino ad n-1
+    int sum2 = 1; // Fibonacci fino ad n-2
+    int x = 0;
+
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        for (int c = 2; c <= n; c++)
+        {
+            x = sum2;
+            sum2 = sum1;
+            sum1 = somma(x, sum2);
+        }
+
+        return sum1;
+    }
+}
+
+int main()
+{
+    cout << "Successore di 5: " << succ(5) << endl;
+
+    cout << "Somma tra 3 e 2: " << somma(3, 2) << endl;
+
+    cout << "Prodotto tra 3 e 2: " << prodotto(3, 2) << endl;
+
+    cout << "Elevamento a potenza tra 3 e 2: " << potenza(3, 2) << endl;
+
+    cout << "Fattoriale di 5: " << fattoriale(5) << endl;
+
+    cout << "Fibonacci di 7: " << fibonacci(7) << endl;
+
+    cout << "Fibonacci iterativa di 7: " << fibonacci_iterativa(7) << endl;
+}
